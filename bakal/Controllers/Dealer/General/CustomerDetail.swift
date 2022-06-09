60eb5a263403408782dbe2b1628d7d8da60dd24d
mfazilkulakoglu <fazilkulakoglu@gmail.com>
2022-06-09 22:51:24 +0300

    first commit

---
+++ bakal/Controllers/Dealer/General/CustomerDetail.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  bakal
+//
+//  Created by Mehmet  Kulakoğlu on 9.06.2022.
+//
+
+import Foundation
---
+++ bakal/Controllers/Dealer/General/ShowMap.swift
@@ -0,0 +1,8 @@
+//
+//  ShowMap.swift
+//  bakal
+//
+//  Created by Mehmet  Kulakoğlu on 9.06.2022.
+//
+
+import Foundation
